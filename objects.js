5658221cb012ffdac0b085d3ac897adf168eae5c
Jonas Ransjö <ransjo@gmail.com>
2014-02-23 01:57:53 +0100

    sprites

--- objects.js
+++ objects.js
@@ -2,6 +2,8 @@
 function Drawable(x, y, width, height, color) {
   Rectangle.call(this, x, y, width, height);
   this.color = color;
+  this.hasSprite = false;
+  this.sprite = null;
 
   this.draw = function (context, cam, wrap) {
     var x = this.x;
@@ -14,18 +16,34 @@ function Drawable(x, y, width, height, color) {
     if (wrap === undefined || wrap == false) {
       x -= cam.pos.x;
       y -= cam.pos.y;
-      context.fillRect(x, y, w, h);
+      if (this.hasSprite)
+	context.drawImage(this.sprite, x, y);
+      else
+	context.fillRect(x, y, w, h);
     } else if (wrap == true) {
       var right = context.canvas.width;
       var bottom = context.canvas.height;
+      var px = cam.grid.projectX(this.x);
+      var py = cam.grid.projectY(this,y);
       
       var rectangles = this.wrap(right, bottom);
       var rect;
       for (var i = 0; i < rectangles.length; i++) {
 	rect = rectangles[i];
-	context.fillRect(rect.x, rect.y, rect.width, rect.height);
+	if (this.hasSprite) 
+	  context.drawImage(this.sprite, cam.grid.projectX(rect.x - px),
+			    cam.grid.projectY(rect.y - py),
+			    rect.width, rect.height,
+			    rect.x, rect.y, rect.width, rect.height);
+	else
+	  context.fillRect(rect.x, rect.y, rect.width, rect.height);
       }
     }
+  };
+  
+  this.addSprite = function (sprite) {
+    this.sprite = sprite;
+    this.hasSprite = true;    
   };
 
 }
